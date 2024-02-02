bc5ce7fdbc2bc1605baf5dd34912aeeebb8ed919
TryConor <cjmw5t@umsystem.edu>
2024-02-02 16:11:57 -0600

    Conor Name + ID

--- HW1/src/HW/Main.java
+++ HW1/src/HW/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Name: Michael Keeney");
 		System.out.println("ID: MichaelKeeney");
-
+		System.out.println("Name: Conor McLaughlin");
+		System.out.println("ID: TryConor");
 	}
 
 }
