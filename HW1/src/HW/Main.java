d6273299e9814fdb53489b0cf6d2db4966b48003
MichaelKeeney <112013713+MichaelKeeney@users.noreply.github.com>
2024-02-02 15:32:30 -0600

    My name and ID

--- HW1/src/HW/Main.java
+++ HW1/src/HW/Main.java
@@ -3,7 +3,8 @@ package HW;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Name: Michael Keeney");
+		System.out.println("ID: MichaelKeeney");
 
 	}
 
